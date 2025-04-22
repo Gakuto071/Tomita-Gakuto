ac63bc5b16d6ae2067a74ea2dd765a48ea9de9f6
gakut <gakut@gakuto>
2025-04-22 21:15:48 +0900

    課題1-1・1-2完了

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -12,8 +12,16 @@ public class Sample {
 		char c;
 		String str;
 		boolean bool;
-
+		
+		b = 0;
+		s = 0;
+		i = 0;
+		l = 0L;
+		f = 0.0f;
+		d = 0.0;
+		c = '\u0000';
+		str = null;
+		bool = false;
 	}
 
 }
-11
