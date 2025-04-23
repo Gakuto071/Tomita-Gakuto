b8a6095a0b301d21d23530c689b970f4a33b6b5a
gakut <gakut@192.168.1.11>
2025-04-23 23:42:41 +0900

    課題1-6終了

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -46,6 +46,17 @@ public class Sample {
 		int num1=23;				
 		System.out.println("ハローJAVA" + (Integer.parseInt(num) + num1));
 		
-	}
+		String name = "山田太郎";
+		int age = 18;
+		double height = 170.5;
+		double weight = 62.2;
+		String favoriteFood = "寿司";
 
-}
+		System.out.println("初めまして" + name + "です");
+        System.out.println("年齢は" + age + "歳です");
+        System.out.println("身長は" + height + "cmです");
+        System.out.println("体重は" + weight + "kgです");
+        System.out.println("好きな食べ物は" + favoriteFood + "です");
+    }
+    }
+		
