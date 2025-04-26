b1e691488b69c695e869a0c77f8c446de02271f3
gakut <gakut@192.168.1.11>
2025-04-26 11:30:17 +0900

    課題1-9終了

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -82,6 +82,24 @@ public class Sample {
         heightInMeters = height / 100;
         System.out.println("BMIは" + weight / (heightInMeters * heightInMeters) + "です");
         
+        
+        age += 24;
+        height += 168.5;
+        weight += 64.2;
+        
+        System.out.println("初めまして" + name + "です");
+        System.out.println("年齢は" + age + "歳です");
+        System.out.println("身長は" + height + "cmです");
+        System.out.println("体重は" + weight + "kgです");
+        System.out.println("好きな食べ物は" + favoriteFood + "です");
+        
+        
+        double heightInMeters1 = 0;
+        heightInMeters1 += height / 100;
+        System.out.println("BMIは" + weight / (heightInMeters1 * heightInMeters1) + "です");
+		
+        
+
 	 }
      }
 		
