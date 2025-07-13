9109f15d5d4b3a0cdcefc4920ba6d02dfdedcd62
gakut <gakut@192.168.1.11>
2025-07-13 14:34:32 +0900

    1-19再提出です

--- src/basicClass/Animal.java
+++ src/basicClass/Animal.java
@@ -4,24 +4,20 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Animal {
-	
-	public static void main(String[] args) {
-		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
-		
-		Dog d = new Dog();
-		System.out.println(d.name);
-		
-		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
-		
-		Dog num = new Dog(2);
-		System.out.println(num.num);
-		
-		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
-		
-		LocalDateTime now = LocalDateTime.now();
-        System.out.println("現在日時: " + now);
+
+    public static void main(String[] args) {
+        // Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力
+        Dog d = new Dog();
+        System.out.println("動物の名前: " + d.getName());
+
+        // Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力
+        Dog num = new Dog(2);
+        System.out.println("動物の数: " + num.getNum());
+
+        // 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力
+        LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd H:m:s");
         String formattedDate = now.format(formatter);
         System.out.println("現在日時: " + formattedDate);
-	}
+    }
 }
--- src/basicClass/Dog.java
+++ src/basicClass/Dog.java
@@ -2,24 +2,39 @@ package basicClass;
 
 public class Dog {
 
-	// Q1：フィールドに動物の名前の変数を定義してください。
-	
-	public String name;
-	    
-	// Q2：フィールドに動物の数の変数を定義してください。
-	
-	public int num;
-	
-	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
-	
-	public Dog() {
-		this.name = "犬";
-	}
-	
-	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
-	
-	public Dog(int num) {
-		this.num = num;
-	}
-}
+    // Q1：フィールドに動物の名前の変数を定義（privateに変更）
+    private String name;
 
+    // Q2：フィールドに動物の数の変数を定義（privateに変更）
+    private int num;
+
+    // Q3：Q1で作成した変数に「犬」を代入するコンストラクタ
+    public Dog() {
+        this.name = "犬";
+    }
+
+    // Q4：Q2で作成した変数に引数を代入するコンストラクタ
+    public Dog(int num) {
+        this.num = num;
+    }
+
+    // name のゲッター
+    public String getName() {
+        return name;
+    }
+
+    // name のセッター
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // num のゲッター
+    public int getNum() {
+        return num;
+    }
+
+    // num のセッター
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
